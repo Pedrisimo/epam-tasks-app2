071c3a400fd528a86d5137fc03aaa53195179a2e
Pedrisimo <artyom.kulyagin@gmail.com>
2016-08-25 17:12:49 +0300

    minor changes

--- controllers/styles.js
+++ controllers/styles.js
@@ -3,8 +3,5 @@ var fs = require('fs'),
 
 exports.getAction = function(request, response) {
     response.writeHead(200, {"Content-Type": 'text/css'});
-    console.log('CSS Styles:' + config.directories.templates + '/css/styles.css');
-    console.log('directories:' + config.directories);
-    console.log('templates:' + config.directories.templates);
     fs.createReadStream(config.directories.templates + '/css/styles.css').pipe(response);
 };
